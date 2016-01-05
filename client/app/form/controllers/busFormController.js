4e1d9ea21335bfff6b9511b06d79e8d5d2608956
Wesley Song <wes.song@gmail.com>
2016-01-04 22:39:51 -0700

    updated business form

--- client/app/form/controllers/busFormController.js
+++ client/app/form/controllers/busFormController.js
@@ -7,5 +7,18 @@
     function busFormController(){
         console.log('load busFormController');
         var vm = this;
+
+        vm.form1 = true;
+        vm.form2 = false;
+
+        vm.form = {
+            premium: 0,
+            sole: 0,
+            worker: 0,
+            vehicle: 0,
+            admin: 0,
+            payroll: 0,
+            employee: 0
+        };
     }
 })();
