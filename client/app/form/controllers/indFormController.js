a0e5b17297405ff96866067ecf1b931e149c6e0e
Wesley Song <wes.song@gmail.com>
2016-01-08 01:41:17 -0700

    updated individual form

--- client/app/form/controllers/indFormController.js
+++ client/app/form/controllers/indFormController.js
@@ -9,5 +9,36 @@
         var vm = this;
 
         vm.form1 = true;
+        vm.form2 = false;
+
+        vm.form = {
+            premium: 0,
+            deductible: 0,
+            copay: 0,
+            yearly: 0,
+            expenses: 0,
+            income: 0
+        };
+        vm.other = null;
+
+        vm.second = {
+            one: 0,
+            two: 0
+        };
+
+        vm.next = function next(){
+            if(vm.other){
+                vm.form1 = false;
+                vm.form2 = true;
+                vm.other = null;
+            }
+            else{
+                vm.form1 = false;
+                vm.form2 = false;
+                vm.result1 = vm.form.premium + vm.form.deductible + vm.form.copy + vm.form.yearly + vm.form.expenses;
+                vm.result2 = vm.form.income * 0.33;
+                vm.difference = vm.result1 - vm.result2;
+            }
+        };
     }
 })();
