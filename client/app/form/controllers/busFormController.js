c238b00229670fd35a341c9171b11417ff0180d1
Wesley Song <wes.song@gmail.com>
2016-01-07 18:07:47 -0700

    updated individual form

--- client/app/form/controllers/busFormController.js
+++ client/app/form/controllers/busFormController.js
@@ -20,5 +20,20 @@
             payroll: 0,
             employee: 0
         };
+
+        vm.next = function next(){
+            console.log('next!');
+            vm.form1 = false;
+        };
+        vm.finish = function finish(){
+            console.log('finish!');
+            vm.form2 = true;
+            vm.healthexpense = vm.form.premium + vm.form.sole + vm.form.worker*0.59 + vm.form.vehicle + vm.form.admin;
+            console.log('healthexpense total', vm.healthexpense);
+            vm.ccexpense = vm.form.payroll + vm.form.employee*0.0333;
+            console.log('ccexpense total', vm.ccexpense);
+            vm.difference = vm.form.healthexpense - vm.form.ccexpense;
+            console.log('difference', vm.difference);
+        };
     }
 })();
