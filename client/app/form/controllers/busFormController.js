3c336e93fae0b5432f4d40f519d31c6817b004e9
Wesley Song <wes.song@gmail.com>
2016-01-12 21:31:19 -0700

    updated individual form and business form with new language

--- client/app/form/controllers/busFormController.js
+++ client/app/form/controllers/busFormController.js
@@ -9,13 +9,10 @@
         var vm = this;
 
         vm.form1 = true;
-        vm.form2 = false;
 
         vm.form = {
             premium: 0,
-            sole: 0,
             worker: 0,
-            vehicle: 0,
             admin: 0,
             payroll: 0,
             employee: 0
@@ -27,8 +24,8 @@
         };
         vm.finish = function finish(){
             console.log('finish!');
-            vm.form2 = true;
-            vm.healthexpense = vm.form.premium + vm.form.sole + vm.form.worker*0.59 + vm.form.vehicle + vm.form.admin;
+            vm.form1 = false;
+            vm.healthexpense = vm.form.premium + vm.form.worker*0.59 + vm.form.admin;
             console.log('healthexpense total', vm.healthexpense);
             vm.ccexpense = vm.form.payroll + vm.form.employee*0.0333;
             console.log('ccexpense total', vm.ccexpense);
--- client/app/form/controllers/indFormController.js
+++ client/app/form/controllers/indFormController.js
@@ -1,15 +1,14 @@
-(function(){
+(function () {
     'use strict';
     angular
         .module('app.form')
         .controller('indFormController', indFormController);
 
-    function indFormController(){
+    function indFormController() {
         console.log('load indFormController');
         var vm = this;
 
         vm.form1 = true;
-        vm.form2 = false;
 
         vm.form = {
             premium: 0,
@@ -19,26 +18,21 @@
             income: 0
         };
         vm.other = null;
+        vm.social = null;
+        vm.medicaid = null;
+        vm.medicare = null;
 
         vm.second = {
             one: 0,
             two: 0
         };
 
-        vm.next = function next(){
-            if(vm.other){
-                vm.form1 = false;
-                vm.form2 = true;
-                vm.other = null;
-            }
-            else{
-                console.log('check form', vm.form);
-                vm.form1 = false;
-                vm.form2 = false;
-                vm.result1 = vm.form.premium * 12 + vm.form.deductible + vm.form.copay + vm.form.expenses;
-                vm.result2 = vm.form.income * 0.0333;
-                vm.difference = vm.result1 - vm.result2;
-            }
+        vm.next = function next() {
+            console.log('check form', vm.form);
+            vm.form1 = false;
+            vm.result1 = vm.form.premium * 12 + vm.form.deductible + vm.form.copay + vm.form.expenses;
+            vm.result2 = vm.form.income * 0.0333;
+            vm.difference = vm.result1 - vm.result2;
         };
     }
 })();
