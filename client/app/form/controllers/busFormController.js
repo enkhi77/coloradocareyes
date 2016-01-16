90d8004352a23a6de3ff6ae82f97e8795cf355c1
Wesley Song <wes.song@gmail.com>
2016-01-16 09:46:51 -0700

    fixing business calculator

--- client/app/form/controllers/busFormController.js
+++ client/app/form/controllers/busFormController.js
@@ -14,8 +14,7 @@
             premium: null,
             worker: null,
             admin: null,
-            payroll: null,
-            employee: null
+            payroll: null
         };
 
         vm.next = function next(){
@@ -27,7 +26,7 @@
             vm.form1 = false;
             var sum1 = vm.form.premium + vm.form.worker*0.59 + vm.form.admin;
             vm.healthexpense = sum1.toFixed(2);
-            var sum2 = vm.form.payroll + vm.form.employee*0.0333;
+            var sum2 = vm.form.payroll*0.067;
             vm.ccexpense = sum2.toFixed(2);
             var sum3 = vm.healthexpense - vm.ccexpense;
             vm.difference = sum3.toFixed(2);
