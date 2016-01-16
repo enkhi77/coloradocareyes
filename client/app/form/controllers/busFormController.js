46eaf44b9f63bfc1d9cbb770858ad8f32239b025
Wesley Song <wes.song@gmail.com>
2016-01-16 12:50:35 -0700

    test double sided live calculations

--- client/app/form/controllers/busFormController.js
+++ client/app/form/controllers/busFormController.js
@@ -8,28 +8,33 @@
         console.log('load busFormController');
         var vm = this;
 
-        vm.form1 = true;
+        function setBusinessForm() {
+            vm.form = {
+                premium: null,
+                worker: null,
+                admin: null,
+                payroll: null
+            };
+        }
+        setBusinessForm();
 
-        vm.form = {
-            premium: null,
-            worker: null,
-            admin: null,
-            payroll: null
-        };
-
-        vm.next = function next(){
+        vm.calculate = function calculate(){
             console.log('next!');
             vm.form1 = false;
+
             var sum1 = vm.form.premium + vm.form.worker*0.59 + vm.form.admin;
             vm.healthexpense = sum1.toFixed(2);
+
             var sum2 = vm.form.payroll*0.067;
             vm.ccexpense = sum2.toFixed(2);
+
             var sum3 = vm.healthexpense - vm.ccexpense;
             vm.difference = sum3.toFixed(2);
         };
 
-        vm.back = function back(){
-            vm.form1 = true;
+        vm.clear = function clear(){
+            setBusinessForm();
+            vm.calculate();
         };
     }
 })();
--- client/app/form/controllers/indFormController.js
+++ client/app/form/controllers/indFormController.js
@@ -8,41 +8,42 @@
         console.log('load indFormController');
         var vm = this;
 
-        vm.form1 = true;
+        function setIndividualForm(){
+            vm.form = {
+                premium: null,
+                deductible: null,
+                copay: null,
+                expenses: null,
+                income: null,
+                SS: null,
+                gross: null,
+                you:{
+                    receiveSS: null,
+                    monthlySS: null,
+                    retirement: null,
+                    age1: null,
+                    age2: null
+                },
+                spouse: {
+                    receiveSS: null,
+                    monthlySS: null,
+                    retirement: null,
+                    age1: null,
+                    age2: null
+                }
+            };
+            vm.other = null;
+            vm.social = null;
+            vm.medicaid = null;
+            vm.medicare = null;
+            vm.filing = null;
+            vm.age1 = null;
+        }
+        setIndividualForm();
 
-        vm.form = {
-            premium: null,
-            deductible: null,
-            copay: null,
-            expenses: null,
-            income: null,
-            SS: null,
-            gross: null,
-            you:{
-                receiveSS: null,
-                monthlySS: null,
-                retirement: null,
-                age1: null,
-                age2: null
-            },
-            spouse: {
-                receiveSS: null,
-                monthlySS: null,
-                retirement: null,
-                age1: null,
-                age2: null
-            }
-        };
-        vm.other = null;
-        vm.social = null;
-        vm.medicaid = null;
-        vm.medicare = null;
-        vm.filing = null;
-        vm.age1 = null;
 
-        vm.next = function next() {
+        vm.calculate = function calculate() {
             console.log('check form', vm.form);
-            vm.form1 = false;
             var ssAnnual = vm.SS * 12;
             var ssFinal = function(){
                 if(ssAnnual - 9000 < 0){
@@ -57,14 +58,17 @@
                 vm.form.copay +
                 vm.form.expenses;
             vm.result1 = sum1.toFixed(2);
+
             var sum2 = vm.form.income * 0.0333;
             vm.result2 = sum2.toFixed(2);
+
             var sum3 = vm.result1 - vm.result2;
             vm.difference = sum3.toFixed(2)
         };
 
-        vm.back = function back() {
-            vm.form1 = true;
+        vm.clear = function clear() {
+            setIndividualForm();
+            vm.calculate();
         };
     }
 })();
