88557fe1fe698c08882c343c74629948b44adac1
Wesley Song <wes.song@gmail.com>
2016-02-20 15:45:27 -0700

    fixed issue with vm.form.gross not being added in because f was not associated with value.

--- client/app/form/controllers/indFormController.js
+++ client/app/form/controllers/indFormController.js
@@ -22,15 +22,13 @@
                     receiveSS: null,
                     monthlySS: null,
                     retirement: null,
-                    age1: null,
-                    age2: null
+                    age: null
                 },
                 spouse: {
                     receiveSS: null,
                     monthlySS: null,
                     retirement: null,
-                    age1: null,
-                    age2: null
+                    age: null
                 }
             };
             vm.other = null;
@@ -91,32 +89,35 @@
 
         var singleCalculation = function singleCalculation() {
             var individualDeductionFinal = indDeduction(vm.form.you.monthlySS, vm.form.you.retirement);
-            if (vm.other && vm.social && vm.form.you.age1 && !vm.form.you.age2) {
+            console.log('individualDeductionFinal', individualDeductionFinal);
+            if (vm.other && vm.social && vm.form.you.age === 'under65') {
                 if (individualDeductionFinal > 20000) {
                     individualDeductionFinal = 20000;
                 }
-                return (vm.form.income * 0.0333) + (0.10 * (f - individualDeductionFinal));
+                return (vm.form.income * 0.0333) + (0.10 * (vm.form.gross - individualDeductionFinal));
             }
-            else if (vm.other && vm.social && vm.form.you.age1 && vm.form.you.age2) {
+            else if (vm.other && vm.social && vm.form.you.age === 'over65') {
                 if (individualDeductionFinal > 24000) {
                     individualDeductionFinal = 24000;
                 }
-                return (vm.form.income * 0.0333) + (0.10 * (f - individualDeductionFinal));
+                return (vm.form.income * 0.0333) + (0.10 * (vm.form.gross - individualDeductionFinal));
             }
         };
         var jointCalculation = function jointCalculation() {
             var jointDeductionFinalYou = jointDeduction(vm.form.you.monthlySS, vm.form.you.retirement, vm.form.spouse.monthlySS);
             var jointDeductionFinalSpouse = jointDeduction(vm.form.spouse.monthlySS, vm.form.spouse.retirement, vm.form.you.monthlySS);
-            if (vm.other && vm.social && vm.form.you.age1 && !vm.form.you.age2 && jointDeductionFinalYou > 20000) {
+            console.log('jointDeductionFinalYou', jointDeductionFinalYou);
+            console.log('jointDeductionFinalSpouse', jointDeductionFinalSpouse);
+            if (vm.other && vm.social && vm.form.you.age === 'under65' && jointDeductionFinalYou > 20000) {
                 jointDeductionFinalYou = 20000;
             }
-            else if (vm.other && vm.social && vm.form.you.age1 && vm.form.you.age2 && jointDeductionFinalYou > 24000) {
+            else if (vm.other && vm.social && vm.form.you.age === 'over65' && jointDeductionFinalYou > 24000) {
                 jointDeductionFinalYou = 24000;
             }
-            if (vm.other && vm.social && vm.form.spouse.age1 && !vm.form.spouse.age2 && jointDeductionFinalSpouse > 20000) {
+            if (vm.other && vm.social && vm.form.spouse.age === 'under65' && jointDeductionFinalSpouse > 20000) {
                 jointDeductionFinalSpouse = 20000;
             }
-            else if (vm.other && vm.social && vm.form.spouse.age1 && vm.form.spouse.age2 && jointDeductionFinalSpouse > 24000) {
+            else if (vm.other && vm.social && vm.form.spouse.age === 'over65' && jointDeductionFinalSpouse > 24000) {
                 jointDeductionFinalSpouse = 24000;
             }
             return (vm.form.income * 0.0333) + (0.10 * (f - jointDeductionFinalYou - jointDeductionFinalSpouse));
