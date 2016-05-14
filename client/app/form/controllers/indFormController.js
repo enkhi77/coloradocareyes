ce2d265576a4c72e9a28029ec7cdb847bf0a7417
Wesley Song <wes.song@gmail.com>
2016-05-14 17:17:05 -0600

    fix calculator max for non-payroll income

--- client/app/form/controllers/indFormController.js
+++ client/app/form/controllers/indFormController.js
@@ -168,6 +168,9 @@
                 vm.result2 = sum2.toFixed(2);
             }
             else if (vm.other && !vm.social) {
+                if(vm.form.gross > 350000 - vm.form.income){
+                    vm.form.gross = 350000 - vm.form.income;
+                }
                 sum2 = (vm.form.income * 0.0333) + (0.10 * vm.form.gross);
                 vm.result2 = sum2.toFixed(2);
             }
